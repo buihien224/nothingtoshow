4b8c3e1325bd9087ef835682d58cdbf4374d8ad8
buihien224 <54226198+buihien224@users.noreply.github.com>
2021-03-05 18:40:01 +0700

    Update Chuoi2.py

--- Chuoi2.py
+++ Chuoi2.py
@@ -1,9 +1,9 @@
 h= input ()
-if h[0].isupper() == True : right = 1 ; wrong = 0
+if h[0].isupper() : right = 1 ; wrong = 0
 else : wrong = 1 ; right = 0
 for c in range(len(h)) :
 	if h[c] == " " : 
-		if h[c+1].isupper() == True : right+=1
+		if h[c+1].isupper() : right+=1
 		else : wrong +=1
 print ("right : " ,right , "wrong : " ,wrong)
 		
