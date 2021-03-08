6213b4295e4cba417fdaf5ba54467916a3ee84e7
buihien224 <54226198+buihien224@users.noreply.github.com>
2021-03-08 11:47:53 +0700

    Update Chuoi2.py
    fix loi dau cach

--- Chuoi2.py
+++ Chuoi2.py
@@ -1,9 +1,23 @@
 h= input ()
-if h[0].isupper() : right = 1 ; wrong = 0
-else : wrong = 1 ; right = 0
-for c in range(len(h)) :
-	if h[c] == " " : 
-		if h[c+1].isupper() : right+=1
-		else : wrong +=1
-print ("right : " ,right , "wrong : " ,wrong)
-		
+if h[0].isupper() : 
+	right = 1 
+	wrong = 0
+elif h[0] == " " : 
+	right = 0 
+	wrong = 0
+else : 
+	wrong = 1 
+	right = 0
+
+try :
+	for c in range(len(h)) :
+		if h[c] == " " : 
+				if h[c+1].isupper() :
+					right+=1
+				else :
+					wrong +=1
+	print ("right :" ,right , "wrong :" ,wrong)
+except : 
+	print ("dont enter space EOL")
+
+ # Bui Duc Hien
